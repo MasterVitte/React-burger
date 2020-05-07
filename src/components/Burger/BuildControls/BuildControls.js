75374cef94c17761bc7f2d49f26509deb5f70747
Artem Vitte <vitte.st13@gmail.com>
2020-05-07 18:41:58 +0400

    Created BuildControl component, realised the buttons of ingredients

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import classes from './BuildControls.css'
+import BuildControl from './BuildControl/BuildControl';
 
 const controls = [
     { label: 'Salad', type: 'salad' },
@@ -13,7 +14,11 @@ const buildControls = (props) => (
     <div className={classes.BuildControls}>
         <p>Current price: <strong>0</strong></p>
         {controls.map(ctrl => (
-            <div>controls</div>
+            <BuildControl
+                key={ctrl.label}
+                label={ctrl.label}
+                type={ctrl.type}
+            />
         ))}
         <button
             className={classes.OrderButton}
