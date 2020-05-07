6ec030ae24e01c6f706895eb7f6de5f87aca103f
Artem Vitte <vitte.st13@gmail.com>
2020-05-07 18:23:36 +0400

    Created Burger component

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import classes from './Burger.css';
+
+const burger = (props) => {
+    return (
+        <div className={classes.Burger}>
+            Burger
+        </div>
+    );
+};
+
+export default burger;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,10 +1,12 @@
 import React from "react";
 
 import Auxiliary from '../../hoc/Auxiliary'
+import Burger from '../../components/Burger/Burger'
 
 const BurgerBuilder = () => {
     return (
         <Auxiliary>
+            <Burger/>
             <div>BurgerBuilder</div>
         </Auxiliary>
     );
