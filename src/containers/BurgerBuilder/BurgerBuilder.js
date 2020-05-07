4d626d6b38787b487e2eb297e5469efbe876f5da
Artem Vitte <vitte.st13@gmail.com>
2020-05-07 18:08:15 +0400

    Created BurgerBuilder

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import Auxiliary from '../../hoc/Auxiliary'
+
+const BurgerBuilder = () => {
+    return (
+        <Auxiliary />
+    );
+};
+
+export default BurgerBuilder;
