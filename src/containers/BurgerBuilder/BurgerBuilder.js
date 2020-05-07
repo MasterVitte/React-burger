9b696ed754a1eaa1cbe65faa06a2e01400ed4a13
Artem Vitte <vitte.st13@gmail.com>
2020-05-07 18:34:17 +0400

    Created state, output burger and added styles for ingredients

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
 
 import Auxiliary from '../../hoc/Auxiliary'
 import Burger from '../../components/Burger/Burger'
 
+const INGREDIENT_PRICES = {
+    salad: 0.5,
+    bacon: 0.4,
+    cheese: 0.3,
+    meat: 0.2
+};
+
 const BurgerBuilder = () => {
+
+    const [state, setState] = useState({
+        ingredients: {
+            salad: 0,
+            bacon: 0,
+            cheese: 0,
+            meat: 0
+        },
+        totalPrice: 0,
+        purchasable: false
+    });
+
     return (
         <Auxiliary>
-            <Burger/>
-            <div>BurgerBuilder</div>
+            <Burger ingredients={state.ingredients}/>
         </Auxiliary>
     );
 };
