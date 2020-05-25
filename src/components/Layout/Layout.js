d69a50567986dadff54d5f3ebf1ec281d7cf0d00
Artem Vitte <vitte.st13@gmail.com>
2020-05-26 00:10:17 +0400

    Added the header and logo

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,9 +2,11 @@ import React from "react";
 
 import Auxiliary from '../../hoc/Auxiliary';
 import classes from './Layout.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 const layout = (props) => (
     <Auxiliary>
+        <Toolbar />
         <main className={classes.Layout}>
             {props.children}
         </main>
