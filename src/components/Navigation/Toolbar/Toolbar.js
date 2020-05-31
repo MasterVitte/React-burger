df25a3db1c5efba579d2db73d77114c88a45cfd1
Artem Vitte <vitte.st13@gmail.com>
2020-05-31 19:10:05 +0400

    Added the functions of open and close mobile menu

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,10 +2,12 @@ import React from "react";
 import Logo from '../../Logo/Logo';
 import classes from './Toolbar.css';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
 
 const toolbar = (props) => {
     return (
         <header className={classes.Toolbar}>
+            <DrawerToggle clicked={props.drawerToggleClicked} />
             <div className={classes.Logo}>
                 <Logo />
             </div>
