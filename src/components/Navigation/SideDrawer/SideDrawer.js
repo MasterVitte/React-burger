e7793e26b9dca7bbc32d15600ae9225d04d622cc
Artem Vitte <vitte.st13@gmail.com>
2020-05-31 18:24:35 +0400

    Added the mobile menu

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,19 +1,20 @@
 import React from "react";
-import Logo from '../../Logo/Logo';
-import classes from './Toolbar.css';
-import NavigationItems from '../NavigationItems/NavigationItems';
 
-const toolbar = (props) => {
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+import classes from './SideDrawer.css';
+
+const sideDrawer = (props) => {
     return (
-        <header className={classes.Toolbar}>
+        <div className={classes.SideDrawer}>
             <div className={classes.Logo}>
                 <Logo />
             </div>
             <nav>
                 <NavigationItems />
             </nav>
-        </header>
+        </div>
     );
 };
 
-export default toolbar;
+export default sideDrawer;
