a4018cb8c3bd2a13f817b20a26be6ed0556a488e
Artem Vitte <vitte.st13@gmail.com>
2020-06-15 01:02:02 +0400

    Added the template of orders

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Layout from './hoc/Layout/Layout'
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 import Checkout from './containers/Checkout/Checkout';
+import Orders from './containers/Orders/Orders';
+
 import { Route, Switch } from 'react-router-dom';
 
 function App() {
@@ -10,6 +12,7 @@ function App() {
             <Layout>
                 <Switch>
                     <Route path="/checkout" component={Checkout} />
+                    <Route path="/orders" component={Orders} />
                     <Route path="/" exact component={BurgerBuilder} />
                 </Switch>
             </Layout>
